e68f583b9c58dcbd81a28f36d6ae0609b3f0fa48
ptincewillopah <proincewillopah@gmail.com>
2024-02-12 02:51:11 +0000

    new push

---
+++ Projects/EC2-Single-Instance/output.tf
@@ -0,0 +1,11 @@
+output "cd--cd-ec2-instance_public_ip" {
+  value = aws_instance.CI-CD-EC2-Instance.public_ip
+}
+
+# output "jenkins_slave1_public_ip" {
+#   value = aws_instance.Jenkin-Slave1-EC2-Instance.public_ip
+# }
+
+# output "ansible_instance_public_ip" {
+#   value = aws_instance.Ansible-EC2-Instance.public_ip
+# }
